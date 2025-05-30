a3c9da3e159f0e28294f753f79289754685e0c65
Ԇ・SAMKIEL <samuelezekiel488@gmail.com>
2025-05-30 06:56:24 +0100

    Updated SAMKIEL Ai

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: '𝕊𝔸𝕄𝕂𝕀𝔼𝕃 𝔹𝕆𝕋',
+  author: '‎',
+  botName: "𝕊𝔸𝕄𝕂𝕀𝔼𝕃 𝔹𝕆𝕋",
+  botOwner: 'Ԇ・SAMKIEL', // Your name
+  ownerNumber: '2348087357158', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.1",
+};
+
+module.exports = settings;

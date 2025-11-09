193e2bf91dd2e9609f94b5388ec784b42c8f668e
SAMKIEL <samuelezekiel488@gmail.com>
2025-11-09 17:04:57 +0100

    update settings.js

--- settings.js
+++ settings.js
@@ -9,10 +9,9 @@ const settings = {
   botNumber: "2348050694825", // Set the bot's WhatsApp number for pairing (without + symbol, just country code & number)
   giphyApiKey: "qnl7ssQChTdPjsKta2Ax2LMaGXz303tq",
   commandMode: "public",
-  description:W
+  description:
     "This is a bot for managing group commands and automating tasks.",
   version: "3.2",
-
 
   updateZipUrl:
     "https://github.com/samkiel488/SAMKIEL-AI/archive/refs/heads/main.zip",
